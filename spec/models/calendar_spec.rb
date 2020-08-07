c1c4f400ad549cf75b97e151328832a488e7b15e
chidi.uba <talk2chidicfc@yahoo.com>
2020-08-07 13:44:08 +0100

    config rspec and write spec test

---
+++ spec/models/calendar_spec.rb
@@ -0,0 +1,28 @@
+require 'date'
+
+RSpec.describe Calendar do
+  before do
+    allow(Date).to receive(:today) { Date.new(2020, 8, 7) }
+  end
+
+  context 'when no argument is given' do
+    subject { described_class.new }
+
+    let(:calendar) do
+<<-EOF
+    August 2020\s\s\s\s\s
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s
+EOF
+    end
+
+    it 'returns the calendar for August with Sunday as the first day of the week' do
+      expect{subject.execute}.to output(calendar).to_stdout
+    end
+  end
+end
