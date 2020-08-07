fe8fed119fd09b3d7887c39d8639207b05461f5b
chidi.uba <talk2chidicfc@yahoo.com>
2020-08-07 14:02:54 +0100

    show calendar for the current month and year when no argument is given

---
+++ models/calendar.rb
@@ -0,0 +1,80 @@
+require 'date'
+
+class Calendar
+  attr_reader :date, :header
+
+  def initialize(first_week_day = 'Su')
+    @date = Date.today
+    @header = week_days(first_week_day)
+  end
+
+  def execute
+    puts output
+  end
+
+  private
+
+  def abbr_week_days
+    @abbr_week_days ||= Date::ABBR_DAYNAMES.map{ |week_day| week_day[0..1] }
+  end
+
+  def week_days(first_week_day)
+    return abbr_week_days if first_week_day == 'Su'
+
+    index_of_day = abbr_week_days.index(first_week_day)
+    abbr_week_days[index_of_day..-1] + abbr_week_days[0...index_of_day]
+  end
+
+  def start_point(date)
+    header.index(
+      date.strftime("%a")[0..1]
+    )
+  end
+
+  def beginning_of_month
+    Date.new(date.year, date.month)
+  end
+
+  def last_day
+    Date.new(date.year, date.month, -1).mday
+  end
+
+  def fill_calendar
+    calendar = Hash.new{ |cal, key| cal[key] = Array.new(7) {''} }
+    week = 0
+    index = start_point(beginning_of_month)
+
+    (1..last_day).each do |day|
+      if index == 7
+        week += 1
+        index = 0
+      end
+      calendar[week][index] = day
+      index += 1
+    end
+
+    calendar
+  end
+
+  def format(day)
+    "%2d" % day.to_i
+  end
+
+  def format_calendar
+    fill_calendar.values.map do |row|
+      row.map { |day| format(day) }.join(" ").gsub(/\s0/, " "*2) << "\n"
+    end.inject(:+)
+  end
+
+  def format_title
+    date.strftime("%B %Y").center(format_header.size)
+  end
+
+  def format_header
+    header.join(' ')
+  end
+
+  def output
+    [format_title, format_header, format_calendar].join("\n")
+  end
+end
